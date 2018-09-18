0fdcd01a2b2ceec32b76100a3b8ebdeb868d5ba4
kristenmt <kc7795@hotmail.com>
2018-09-17 21:27:17 -0400

    fixed images, some css

--- src/components/PuppyCard/PuppyCard.js
+++ src/components/PuppyCard/PuppyCard.js
@@ -2,33 +2,17 @@ import React from "react";
 //import PuppyBox from "../PuppyBox";
 import "./PuppyCard.css";
 
-const PuppyCard = (props) => (
+const PuppyCard = props => (
     <div className="card">
         <div className="img-container">
-        {/* need to add onclick for scoring */}
-            {/* {props.list.map((item,i) => { */}
-                {/* return ( */}
-                    {/* // <PuppyBox */}
-                    {/* //     key={item.breed} */}
-                    {/* //     image={item.image} */}
-                    {/* //     /> */}
-                {/* // ) */}
-            {/* // } */}
-        {/* ) */}
-    {/* } */}
-{/* <a onClick={() => props.selectPuppy(props.id)}
-    className={props.curScore === 0 }
-    > */}
-            {/* <img alt={props.id} src={props.image} />
-            </a> */}
 
-<a>
-            <img alt={props.id} src={props.image} />
+            <a onClick={() => props.selectPuppy(props.id)}
+                className={props.curScore === 0 ? "style_prevu_kit style_prevu_kit_ex" : "style_prevu_kit"}
+            >
+                <img alt={props.id} src={props.image} />
             </a>
-
-
         </div>
-        </div>
+    </div>
 )
 
 export default PuppyCard;
--- src/components/Title/Title.js
+++ src/components/Title/Title.js
@@ -4,7 +4,8 @@ import "./Title.css";
 const Title = props => (
     <div className="gametitle">
         <h1>Puppy Click Game!</h1>
-        <p>Click on an image to get a point. Remember which one you clicked, because if you click it again, you lose the game!</p>
+        <p>Click on an image to get a point.</p>
+        <p>Remember which one you clicked, because if you click it again, you lose the game!</p>
     </div>
 );
 
